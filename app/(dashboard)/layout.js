a5945e99d5f669e6dbda989edebec68f846dc213
Shreyansh <sahaishreyansh@gmail.com>
2024-10-15 20:20:26 +0530

    Initial commit

---
+++ app/(dashboard)/layout.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const metadata = {
+    title: "ToursGPT",
+    description: "GPT for Tours: Find must-visit places in any city instantly!",
+};
+
+const layout = ({ children }) => {
+    return <div>{children}</div>;
+};
+
+export default layout;
